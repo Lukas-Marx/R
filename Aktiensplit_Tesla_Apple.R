b5365a15ef5b6170a3f89861a82913142a830034
Lukas-Marx <70298828+Lukas-Marx@users.noreply.github.com>
2020-09-12 22:11:25 +0200

    subsequence 1 und 2 auswertungen

--- Aktiensplit_Tesla_Apple.R
+++ Aktiensplit_Tesla_Apple.R
@@ -87,3 +87,42 @@ Aktie.Adjusted2sub2 = get.hist.quote(instrument = Aktie2,
                                      quote = "Adjusted", provider = "yahoo",
                                      compression = "d"
 )
+
+ERW_AAPL_whole_vol = mean (Aktie.volume1)
+ERW_TSLA_whole_vol = mean(Aktie.volume2)
+Var_AAPL_single_vol = (Aktie.volume1 - ERW_AAPL_whole_vol)^2
+Var_AAPL_whole_vol = sum(Var_AAPL_single_vol) / length(Var_AAPL_single_vol)
+StabW_AAPL_whole_vol = sqrt(Var_AAPL_whole_vol)
+Stabw_AAPL_whole_vol_up = ERW_AAPL_whole_vol + StabW_AAPL_whole_vol
+Stabw_AAPL_whole_vol_down = ERW_AAPL_whole_vol - StabW_AAPL_whole_vol
+Var_TSLA_single_vol = (Aktie.volume2 - ERW_TSLA_whole_vol)^2
+Var_TSLA_whole_vol = sum(Var_TSLA_single_vol) / length(Var_TSLA_single_vol)
+StabW_TSLA_whole_vol = sqrt(Var_TSLA_whole_vol)
+Stabw_TSLA_whole_vol_up = ERW_TSLA_whole_vol + StabW_TSLA_whole_vol
+Stabw_TSLA_whole_vol_down = ERW_TSLA_whole_vol - StabW_TSLA_whole_vol
+
+ERW_AAPL_whole_vol_sub1 = mean (Aktie.volume1sub1)
+ERW_TSLA_whole_vol_sub1 = mean(Aktie.volume2sub1)
+Var_AAPL_single_vol_sub1 = (Aktie.volume1sub1 - ERW_AAPL_whole_vol_sub1)^2
+Var_AAPL_whole_vol_sub1 = sum(Var_AAPL_single_vol_sub1) / length(Var_AAPL_single_vol_sub1)
+StabW_AAPL_whole_vol_sub1 = sqrt(Var_AAPL_whole_vol_sub1)
+Stabw_AAPL_whole_vol_sub1_up = ERW_AAPL_whole_vol_sub1 + StabW_AAPL_whole_vol_sub1
+Stabw_AAPL_whole_vol_sub1_down = ERW_AAPL_whole_vol_sub1 - StabW_AAPL_whole_vol_sub1
+Var_TSLA_single_vol_sub1 = (Aktie.volume2sub1 - ERW_TSLA_whole_vol_sub1)^2
+Var_TSLA_whole_vol_sub1 = sum(Var_TSLA_single_vol_sub1) / length(Var_TSLA_single_vol_sub1)
+StabW_TSLA_whole_vol_sub1 = sqrt(Var_TSLA_whole_vol_sub1)
+Stabw_TSLA_whole_vol_sub1_up = ERW_TSLA_whole_vol_sub1 + StabW_TSLA_whole_vol_sub1
+Stabw_TSLA_whole_vol_sub1_down = ERW_TSLA_whole_vol_sub1 - StabW_TSLA_whole_vol_sub1
+
+ERW_AAPL_whole_vol_sub2 = mean (Aktie.volume1sub2)
+ERW_TSLA_whole_vol_sub2 = mean(Aktie.volume2sub2)
+Var_AAPL_single_vol_sub2 = (Aktie.volume1sub2 - ERW_AAPL_whole_vol_sub2)^2
+Var_AAPL_whole_vol_sub2 = sum(Var_AAPL_single_vol_sub2) / length(Var_AAPL_single_vol_sub2)
+StabW_AAPL_whole_vol_sub2 = sqrt(Var_AAPL_whole_vol_sub2)
+Stabw_AAPL_whole_vol_sub2_up = ERW_AAPL_whole_vol_sub2 + StabW_AAPL_whole_vol_sub2
+Stabw_AAPL_whole_vol_sub2_down = ERW_AAPL_whole_vol_sub2 - StabW_AAPL_whole_vol_sub2
+Var_TSLA_single_vol_sub2 = (Aktie.volume2sub2 - ERW_TSLA_whole_vol_sub2)^2
+Var_TSLA_whole_vol_sub2 = sum(Var_TSLA_single_vol_sub2) / length(Var_TSLA_single_vol_sub2)
+StabW_TSLA_whole_vol_sub2 = sqrt(Var_TSLA_whole_vol_sub2)
+Stabw_TSLA_whole_vol_sub2_up = ERW_TSLA_whole_vol_sub2 + StabW_TSLA_whole_vol_sub2
+Stabw_TSLA_whole_vol_sub2_down = ERW_TSLA_whole_vol_sub2 - StabW_TSLA_whole_vol_sub2
