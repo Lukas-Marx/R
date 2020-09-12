0343d24b09097b21d85b4406c5e256e0586a7d93
Lukas-Marx <70298828+Lukas-Marx@users.noreply.github.com>
2020-09-12 22:24:04 +0200

    added adjusted stock profil

--- Aktiensplit_Tesla_Apple.R
+++ Aktiensplit_Tesla_Apple.R
@@ -157,3 +157,73 @@ abline(h = Stabw_TSLA_whole_vol_sub2_up, lty =2, col= "red")
 abline(h = Stabw_TSLA_whole_vol_sub2_down, lty =2, col= "red")
 abline(h = ERW_TSLA_whole_vol_sub2, col= "red")
 legend("topleft", legend = c("EV_pre", "SD_pre","EV_post","SD_post"), col = c("blue","blue","red","red"), lty = 1:2,cex = 0.6)
+
+ERW_AAPL_whole_adj = mean (Aktie.Adjusted1)
+ERW_TSLA_whole_adj = mean(Aktie.Adjusted2)
+Var_AAPL_single_adj = (Aktie.Adjusted1 - ERW_AAPL_whole_adj)^2
+Var_AAPL_whole_adj = sum(Var_AAPL_single_adj) / length(Var_AAPL_single_adj)
+StabW_AAPL_whole_adj = sqrt(Var_AAPL_whole_adj)
+Stabw_AAPL_whole_adj_up = ERW_AAPL_whole_adj + StabW_AAPL_whole_adj
+Stabw_AAPL_whole_adj_down = ERW_AAPL_whole_adj - StabW_AAPL_whole_adj
+Var_TSLA_single_adj = (Aktie.Adjusted2 - ERW_TSLA_whole_adj)^2
+Var_TSLA_whole_adj = sum(Var_TSLA_single_adj) / length(Var_TSLA_single_adj)
+StabW_TSLA_whole_adj = sqrt(Var_TSLA_whole_adj)
+Stabw_TSLA_whole_adj_up = ERW_TSLA_whole_adj + StabW_TSLA_whole_adj
+Stabw_TSLA_whole_adj_down = ERW_TSLA_whole_adj - StabW_TSLA_whole_adj
+
+ERW_AAPL_whole_adj_sub1 = mean (Aktie.Adjusted1sub1)
+ERW_TSLA_whole_adj_sub1 = mean(Aktie.Adjusted2sub1)
+Var_AAPL_single_adj_sub1 = (Aktie.Adjusted1sub1 - ERW_AAPL_whole_adj_sub1)^2
+Var_AAPL_whole_adj_sub1 = sum(Var_AAPL_single_adj_sub1) / length(Var_AAPL_single_adj_sub1)
+StabW_AAPL_whole_adj_sub1 = sqrt(Var_AAPL_whole_adj_sub1)
+Stabw_AAPL_whole_adj_sub1_up = ERW_AAPL_whole_adj_sub1 + StabW_AAPL_whole_adj_sub1
+Stabw_AAPL_whole_adj_sub1_down = ERW_AAPL_whole_adj_sub1 - StabW_AAPL_whole_adj_sub1
+Var_TSLA_single_adj_sub1 = (Aktie.Adjusted2sub1 - ERW_TSLA_whole_adj_sub1)^2
+Var_TSLA_whole_adj_sub1 = sum(Var_TSLA_single_adj_sub1) / length(Var_TSLA_single_adj_sub1)
+StabW_TSLA_whole_adj_sub1 = sqrt(Var_TSLA_whole_adj_sub1)
+Stabw_TSLA_whole_adj_sub1_up = ERW_TSLA_whole_adj_sub1 + StabW_TSLA_whole_adj_sub1
+Stabw_TSLA_whole_adj_sub1_down = ERW_TSLA_whole_adj_sub1 - StabW_TSLA_whole_adj_sub1
+
+ERW_AAPL_whole_adj_sub2 = mean (Aktie.Adjusted1sub2)
+ERW_TSLA_whole_adj_sub2 = mean(Aktie.Adjusted2sub2)
+Var_AAPL_single_adj_sub2 = (Aktie.Adjusted1sub2 - ERW_AAPL_whole_adj_sub2)^2
+Var_AAPL_whole_adj_sub2 = sum(Var_AAPL_single_adj_sub2) / length(Var_AAPL_single_adj_sub2)
+StabW_AAPL_whole_adj_sub2 = sqrt(Var_AAPL_whole_adj_sub2)
+Stabw_AAPL_whole_adj_sub2_up = ERW_AAPL_whole_adj_sub2 + StabW_AAPL_whole_adj_sub2
+Stabw_AAPL_whole_adj_sub2_down = ERW_AAPL_whole_adj_sub2 - StabW_AAPL_whole_adj_sub2
+Var_TSLA_single_adj_sub2 = (Aktie.Adjusted2sub2 - ERW_TSLA_whole_adj_sub2)^2
+Var_TSLA_whole_adj_sub2 = sum(Var_TSLA_single_adj_sub2) / length(Var_TSLA_single_adj_sub2)
+StabW_TSLA_whole_adj_sub2 = sqrt(Var_TSLA_whole_adj_sub2)
+Stabw_TSLA_whole_adj_sub2_up = ERW_TSLA_whole_adj_sub2 + StabW_TSLA_whole_adj_sub2
+Stabw_TSLA_whole_adj_sub2_down = ERW_TSLA_whole_adj_sub2 - StabW_TSLA_whole_adj_sub2
+
+
+plot(Aktie.Adjusted1, xlab="gesamter Zeitraum", ylab='Adjusted Stock AAPL (purple)', col="purple")
+abline(h = Stabw_AAPL_whole_adj_up, lty =2, col= "blue")
+abline(h = Stabw_AAPL_whole_adj_down, lty =2, col= "blue")
+abline(h = ERW_AAPL_whole_adj, col= "blue")
+legend("topleft", legend = c("EV", "SD"), col = c("blue","blue"), lty = 1:2,cex = 0.6)
+
+plot(Aktie.Adjusted2, xlab="gesamter Zeitraum", ylab='Adjusted Stock TSLA (purple)', col="purple")
+abline(h = Stabw_TSLA_whole_adj_up, lty =2, col= "blue")
+abline(h = Stabw_TSLA_whole_adj_down, lty =2, col= "blue")
+abline(h = ERW_TSLA_whole_adj, col= "blue")
+legend("topleft", legend = c("EV", "SD"), col = c("blue","blue"), lty = 1:2,cex = 0.6)
+
+plot(Aktie.Adjusted1, xlab="gesamter Zeitraum", ylab='Adjusted Stock AAPL (purple)', col="purple")
+abline(h = Stabw_AAPL_whole_adj_sub1_up, lty =2, col= "blue")
+abline(h = Stabw_AAPL_whole_adj_sub1_down, lty =2, col= "blue")
+abline(h = ERW_AAPL_whole_adj_sub1, col= "blue")
+abline(h = Stabw_AAPL_whole_adj_sub2_up, lty =2, col= "red")
+abline(h = Stabw_AAPL_whole_adj_sub2_down, lty =2, col= "red")
+abline(h = ERW_AAPL_whole_adj_sub2, col= "red")
+legend("topleft", legend = c("EV_pre", "SD_pre","EV_post","SD_post"), col = c("blue","blue","red","red"), lty = 1:2,cex = 0.6)
+
+plot(Aktie.Adjusted2, xlab="gesamter Zeitraum", ylab='Adjusted Stock TSLA (purple)', col="purple")
+abline(h = Stabw_TSLA_whole_adj_sub1_up, lty =2, col= "blue")
+abline(h = Stabw_TSLA_whole_adj_sub1_down, lty =2, col= "blue")
+abline(h = ERW_TSLA_whole_adj_sub1, col= "blue")
+abline(h = Stabw_TSLA_whole_adj_sub2_up, lty =2, col= "red")
+abline(h = Stabw_TSLA_whole_adj_sub2_down, lty =2, col= "red")
+abline(h = ERW_TSLA_whole_adj_sub2, col= "red")
+legend("topleft", legend = c("EV_pre", "SD_pre","EV_post","SD_post"), col = c("blue","blue","red","red"), lty = 1:2,cex = 0.6)
