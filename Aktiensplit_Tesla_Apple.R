ac576b155ae9983998e5cc623d91876e594ebd87
Lukas-Marx <70298828+Lukas-Marx@users.noreply.github.com>
2020-09-12 21:59:10 +0200

    add changes

--- Aktiensplit_Tesla_Apple.R
+++ Aktiensplit_Tesla_Apple.R
@@ -3,14 +3,14 @@
 library(tseries)
 library(zoo)
 
-Start = "2019-03-11"
-Ende = "2020-09-01"
+Start = "2020-07-30"
+Ende = "2020-09-30"
 
-Subsequence1.Start= "2020-03-11"
+Subsequence1.Start= "2020-07-30"
 Subsequence1.Ende = "2020-08-31"
 
 Subsequence2.Start = "2019-09-01"
-Subsequence2.Ende ="2020-03-10"
+Subsequence2.Ende ="2020-09-30"
 
 Aktie1 = "AAPL"
 Aktie2 = "TSLA"
@@ -39,3 +39,51 @@ Aktie.Adjusted2 = get.hist.quote(instrument = Aktie2,
                                  compression = "d"
 )
 
+
+Aktie.volume1sub1 = get.hist.quote(instrument = Aktie1, 
+                               start = Subsequence1.Start, end = Subsequence1.Ende,
+                               quote = "Volume", provider = "yahoo",
+                               compression = "d"
+)
+
+Aktie.volume2sub1 = get.hist.quote(instrument = Aktie2, 
+                               start = Subsequence1.Start, end = Subsequence1.Ende,
+                               quote = "Volume", provider = "yahoo",
+                               compression = "d"
+)
+
+Aktie.Adjusted1sub1 = get.hist.quote(instrument = Aktie1, 
+                                 start = Subsequence1.Start, end = Subsequence1.Ende,
+                                 quote = "Adjusted", provider = "yahoo",
+                                 compression = "d"
+)
+
+Aktie.Adjusted2sub1 = get.hist.quote(instrument = Aktie2, 
+                                 start = Subsequence1.Start, end = Subsequence1.Ende,
+                                 quote = "Adjusted", provider = "yahoo",
+                                 compression = "d"
+)
+
+Aktie.volume1sub2 = get.hist.quote(instrument = Aktie1, 
+                                   start = Subsequence2.Start, end = Subsequence2.Ende,
+                                   quote = "Volume", provider = "yahoo",
+                                   compression = "d"
+)
+
+Aktie.volume2sub2 = get.hist.quote(instrument = Aktie2, 
+                                   start = Subsequence2.Start, end = Subsequence2.Ende,
+                                   quote = "Volume", provider = "yahoo",
+                                   compression = "d"
+)
+
+Aktie.Adjusted1sub2 = get.hist.quote(instrument = Aktie1, 
+                                     start = Subsequence2.Start, end = Subsequence2.Ende,
+                                     quote = "Adjusted", provider = "yahoo",
+                                     compression = "d"
+)
+
+Aktie.Adjusted2sub2 = get.hist.quote(instrument = Aktie2, 
+                                     start = Subsequence2.Start, end = Subsequence2.Ende,
+                                     quote = "Adjusted", provider = "yahoo",
+                                     compression = "d"
+)
