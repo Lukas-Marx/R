775ea303f6da076470acb5c141d45efb62b25f55
Lukas-Marx <70298828+Lukas-Marx@users.noreply.github.com>
2020-10-02 18:31:31 +0200

    introduce a new thing

---
+++ Siemens_Carving_action.R
@@ -0,0 +1,11 @@
+#Lukas Marx, 11.09.2020 Fürth, Friedrich-Alexander-Universität Erlangen-Nuernberg
+#Lukas.Marx@Fau.de
+library(tseries)
+library(zoo)
+
+Start = "2017-01-01"
+Ende = "2020-10-03"
+
+Health.Start= "2018-03-16"
+
+energy.Start = "2020-09-28"
